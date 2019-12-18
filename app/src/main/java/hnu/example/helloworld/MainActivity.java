ec67dc461327ec000cbbaa3899271a8aba3526c6
dany meyer <dany.meyer@hs-neu-ulm.de>
2019-12-18 20:37:54 +0100

    implement logic for button

--- app/src/main/java/hnu/example/helloworld/MainActivity.java
+++ app/src/main/java/hnu/example/helloworld/MainActivity.java
@@ -3,12 +3,36 @@ package hnu.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
+    private boolean toogle = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text1);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (toogle) {
+                    mTextView.setText("");
+                } else {
+                    mTextView.setText("Hallo");
+                }
+                toogle = !toogle;
+
+            }
+        });
     }
 }
